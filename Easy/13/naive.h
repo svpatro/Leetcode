2f3c76f2e853656013a03f2af6ab0d8551cc4532
Shamik Patro <patroshamik@gmail.com>
2024-12-13 10:36:49 -0600

    Altered 13 with switch cases

--- Easy/13/naive.h
+++ Easy/13/naive.h
@@ -1,7 +1,12 @@
-// Problem: Roman to Integer
-// O(s) Solution
-// IV, IX, XL, XC, CD, CM
-// 4, 9, 40, 90, 400, 900
+/* 
+Problem: Roman to Integer
+O(s) Solution
+
+NOTE:
+Switch cases are slower than if statements by 3ms.
+The if statement implementation can be found at 
+commit: 168697ba10b3da60cdc17414f07c40473c7a1d23
+*/
 
 #include <string>
 
@@ -9,45 +14,59 @@ class Naive {
 public:
     int romanToInt(std::string s) {
 
+        // Declare necessary variables
+
         int ctr = 0;
         std::string state = s;
         char prev;
         char current;
 
+        // Take care of subtraction: IV, IX, XL, XC, CD, CM
+
         for (int i = 1; i < s.length(); i++){
             prev = s[i-1];
             current = s[i];
-
-            if (prev == 'I' && current == 'V'){
-                ctr += 4;
-                state.replace(i-1, 2, 2, '_');
-            }
-            
-            if (prev == 'I' && current == 'X'){
-                ctr += 9;
-                state.replace(i-1, 2, 2, '_');
-            }
-
-            if (prev == 'X' && current == 'L'){
-                ctr += 40;
-                state.replace(i-1, 2, 2, '_');
-            }
-            
-            if (prev == 'X' && current == 'C'){
-                ctr += 90;
-                state.replace(i-1, 2, 2, '_');
-            }
-
-            if (prev == 'C' && current == 'D'){
-                ctr += 400;
-                state.replace(i-1, 2, 2, '_');
-            }
-
-            if (prev == 'C' && current == 'M'){
-                ctr += 900;
-                state.replace(i-1, 2, 2, '_');
+            switch(prev){
+                case 'I':
+                    switch(current){
+                        case 'V':
+                            ctr += 4;
+                            state.replace(i-1, 2, 2, '_');
+                            break;
+                        case 'X':
+                            ctr += 9;
+                            state.replace(i-1, 2, 2, '_');
+                            break;
+                    }
+                    break;
+                case 'X':
+                    switch(current){
+                        case 'L':
+                            ctr += 40;
+                            state.replace(i-1, 2, 2, '_');
+                            break;
+                        case 'C':
+                            ctr += 90;
+                            state.replace(i-1, 2, 2, '_');
+                            break;
+                    }
+                    break;
+                case 'C':
+                    switch(current){
+                        case 'D':
+                            ctr += 400;
+                            state.replace(i-1, 2, 2, '_');
+                            break;
+                        case 'M':
+                            ctr += 900;
+                            state.replace(i-1, 2, 2, '_');
+                            break;
+                    }
+                    break;
             }
         }
+
+        // For any remaining letters, add to counter
 
         for (int i = 0; i < state.length(); i++){
             switch(state[i]){
